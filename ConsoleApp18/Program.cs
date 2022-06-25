103d48768df0415c3642986159c2e7d6a4c5cb90
kopikiadu <alfidlo@centrum.cz>
2022-06-26 00:14:21 +0200

    added more functionality and removed dependence on string arrays

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -2,16 +2,46 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
 namespace ConsoleApp18
 {
 
     class Program
     {
-        static List<Name> namebank = new();
+        static Random rand = new Random();
+
+        private static Name GetName()
+        { 
+            return Namebank[rand.Next(Namebank.Count)]; 
+        }
+        private static string GetVehicle()
+        {
+            var loc = GetName().Locative;
+            string v = $"v {loc}";
+            if (loc.ToLower().StartsWith('f') || loc.ToLower().StartsWith('v'))
+                v = $"ve {loc}";
+            string[] results = { GetName().Instrumental, v, $"na {GetName().Locative}" };
+            return results[rand.Next(results.Length)];
+        }
+        static List<Name> Namebank = new Name[]
+        {
+            new Name("hjuh", "hjuhu", "hjuhu", "hjuh", "hjuhu", "hjuhu", "hjuhem"),
+            new Name("Jawouwel", "Jawouwela", "Jawouwelovi", "Jawouwela", "Jawouwele", "Jawouwelovi", "Jawouwelem"),
+            new Name("Tomášel", "Tomášela", "Tomášelovi", "Tomášela", "Tomášeli", "Tomášelovi", "Tomášelem"),
+            new Name("Adámel", "Adámela", "Adámelovi", "Adámela", "Adámele", "Adámelovi", "Adámelem"),
+            new Name("tvoje máma", "tvojí mámy", "tvojí mámě", "tvojí mámu", "tvoje mámo", "tvojí mámě", "tvojí mámou"),
+            new Name("Fousová", "Fousový", "Fousový", "Fousovou", "Fousová", "Fousový", "Fousovou"),
+            new Name("tdymence", "tdymence", "tdymenci", "tdymenci", "tdymence", "tdymenci", "tdymencí"),
+            new Name("tvoje prdel", "tvojí prdele", "tvojí prdeli", "tvojí prdel", "tvoje prdeli", "tvojí prdeli", "tvojí prdelí")
+        }
+        .ToList();
         static void Main(string[] args)
         {
-            var rand = new Random();
+
+
+            
             string[] locs = { "hjuhu", "tdymence", "Tomáše", "tvojí mámy", "komínela", "Brazílie" };
             string[] things = { "tvojí mámou", "tdymencí", "gulášem", "hjuhem" };
             string[] s1 = { "v tvojí mámě", "ve fousový", "v hjuhu", "v guláši" };
@@ -21,12 +51,13 @@ namespace ConsoleApp18
             string veh = vehs[rand.Next(vehs.Length)];
             string person = things[rand.Next(things.Length)];
             string person2 = people[rand.Next(people.Length)];
-            string[] phrases = { $"Jedeš \ns {person} \ndo {loc}"
-                    , $"Jedeš \n{veh} \ndo {loc}"
-                    , $"{person2}\njede \ns {person} \ndo {loc}"
-                    , $"{person2}\n jede\n{veh} \ndo {loc}"
+            string[] phrases = { $"Jedeš \ns {GetName().Instrumental} \ndo {GetName().Genitive}"
+                    , $"Jedeš \n{GetVehicle()} \ndo {GetName().Genitive}"
+                    , $"{GetName().Nominative}\njede \ns {GetName().Instrumental} \ndo {GetName().Genitive}"
+                    , $"{GetName().Nominative}\n jede\n{GetVehicle()} \ndo {GetName().Genitive}"
                     , $"*Strok*"
-                    , $"{person2}\nhjuhne\ndo {loc}"};
+                    , $"{GetName().Nominative}\nhjuhne\ndo {GetName().Genitive}"
+                    , $"{GetName().Nominative}\nje\n{GetName().Nominative}"};
             string phrase = phrases[rand.Next(phrases.Length)];
             string[] affixes = { "\nJúúúú\n", "\nHjuh\n", "\nRAF\n","\nRAF Hmmm\n","\nHmmmmmm\n", null, null, null, null};
             string prefix = affixes[rand.Next(affixes.Length)];
